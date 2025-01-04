338e4a80de39325aae0cf9a0c06168287e33939c
iamacoffeepot <me@iamateapot.dev>
2025-01-04 04:12:17 -0700

    feat: implement get index of lsb

--- src/libraries/BitmathLibrary.sol
+++ src/libraries/BitmathLibrary.sol
@@ -6,5 +6,44 @@ library BitmathLibrary {
     /// @custom:todo
     function getIndexOfLsb(uint256 x) internal pure returns (uint8 result) {
         require(x > 0);
+
+        unchecked {
+            if ((x & 0xffffffffffffffffffffffffffffffff) == 0) {
+                result += 128;
+                x >>= 128;
+            }
+
+            if ((x & 0xffffffffffffffff) == 0) {
+                result += 64;
+                x >>= 64;
+            }
+
+            if ((x & 0xffffffff) == 0) {
+                result += 32;
+                x >>= 32;
+            }
+
+            if ((x & 0xffff) == 0) {
+                result += 16;
+                x >>= 16;
+            }
+
+            if ((x & 0xff) == 0) {
+                result += 8;
+                x >>= 8;
+            }
+
+            if ((x & 0xf) == 0) {
+                result += 4;
+                x >>= 4;
+            }
+
+            if ((x & 0x3) == 0) {
+                result += 2;
+                x >>= 2;
+            }
+
+            result += 1;
+        }
     }
 }
