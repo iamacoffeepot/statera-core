686f2899f98ccf1a3fa219b6ac4177f8ade91780
iamacoffeepot <me@iamateapot.dev>
2025-01-04 23:14:05 -0700

    refactor: rename fixed point math library

--- src/libraries/FixedPointMathLibrary.sol
+++ src/libraries/FixedPointMathLibrary.sol
@@ -9,7 +9,7 @@ import {
 uint256 constant UINT8_MAXIMUM = 0xff;
 
 /// @notice A collection of functions for performing operations on fixed point numbers.
-library FixedPointLibrary {
+library FixedPointMathLibrary {
     /// @notice Multiplies an unsigned 256 bit integer by a binary fixed point number with 4 integer bits and 4 fraction bits.
     function multiplyByQ4x4(uint256 n, Q4x4 q) internal pure returns (uint256 result) {
         require(n < 2**248, KernelError(KernelErrorType.ILLEGAL_ARGUMENT));
