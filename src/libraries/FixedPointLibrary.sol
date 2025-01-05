b877227fd4f8928a8ea16945bc11836a4dc68306
iamacoffeepot <me@iamateapot.dev>
2025-01-04 23:11:51 -0700

    feat: add multiply uint256 by Q4x4

--- src/libraries/FixedPointLibrary.sol
+++ src/libraries/FixedPointLibrary.sol
@@ -5,7 +5,17 @@ import {
     KernelErrorType
 } from "../types/Types.sol";
 
+/// @dev Alias of `type(uint8).max` for use in inline assembly.
+uint256 constant UINT8_MAXIMUM = 0xff;
+
 /// @notice A collection of functions for performing operations on fixed point numbers.
 library FixedPointLibrary {
+    /// @notice Multiplies an unsigned 256 bit integer by a binary fixed point number with 4 integer bits and 4 fraction bits.
+    function multiplyByQ4x4(uint256 n, Q4x4 q) internal pure returns (uint256 result) {
+        require(n < 2**248, KernelError(KernelErrorType.ILLEGAL_ARGUMENT));
 
+        assembly {
+            result := shr(4, mul(and(UINT8_MAXIMUM, q), n))
+        }
+    }
 }
