fde03b08304e151faf33ae34368b6c4abc212a28
iamacoffeepot <me@iamateapot.dev>
2025-01-05 03:08:42 -0700

    fix: broken import

--- src/libraries/BucketLibrary.sol
+++ src/libraries/BucketLibrary.sol
@@ -2,6 +2,10 @@ pragma solidity 0.8.27;
 
 import {TokenizedVault} from "../interfaces/TokenizedVault.sol";
 
+import {
+    Bucket
+} from "../types/Types.sol";
+
 /// @notice A collection of functions for performing operations on a `Bucket`.
 library BucketLibrary {
     /// @custom:todo
