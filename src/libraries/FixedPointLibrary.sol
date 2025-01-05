07b4e4696aa80c9fc0de83a16533ba44619cee7d
iamacoffeepot <me@iamateapot.dev>
2025-01-04 18:46:22 -0700

    feat: add fixed point library stub

---
+++ src/libraries/FixedPointLibrary.sol
@@ -0,0 +1,11 @@
+pragma solidity 0.8.27;
+
+import {
+    KernelError,
+    KernelErrorType
+} from "../types/Types.sol";
+
+/// @notice A collection of functions for performing operations on fixed point numbers.
+library FixedPointLibrary {
+
+}
