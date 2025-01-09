fc7c29a5be49b64fb6728215d720cf6f9488e9fd
iamacoffeepot <me@iamateapot.dev>
2025-01-09 00:28:19 -0700

    feat: export d256x18

--- src/types/Types.sol
+++ src/types/Types.sol
@@ -5,4 +5,5 @@ import {Commitment} from "./Commitment.sol";
 import {KernelError, KernelErrorType} from "./KernelError.sol";
 import {LendingTerms, LendingTermsPacked} from "./LendingTerms.sol";
 import {BitmapX256} from "./bitmap/BitmapX256.sol";
+import {D256x18} from "./fixed/D256x18.sol";
 import {Q4x4, Q4X4_ONE} from "./fixed/Q4x4.sol";
