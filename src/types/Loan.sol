58392eb405e03e96ba310209f44fd15661f610fb
iamacoffeepot <me@iamateapot.dev>
2025-01-14 03:54:17 -0700

    feat: add loan struct

---
+++ src/types/Loan.sol
@@ -0,0 +1,5 @@
+pragma solidity 0.8.27;
+
+struct Loan {
+    uint256 placeholder;
+}
--- src/types/Types.sol
+++ src/types/Types.sol
@@ -4,6 +4,7 @@ import {Bucket} from "./Bucket.sol";
 import {Commitment} from "./Commitment.sol";
 import {KernelError, KernelErrorType} from "./KernelError.sol";
 import {LendingTerms, LendingTermsPacked} from "./LendingTerms.sol";
+import {Loan} from "./Loan.sol";
 import {BitmapX256} from "./bitmap/BitmapX256.sol";
 import {D256x18} from "./fixed/D256x18.sol";
 import {Q4x4, Q4X4_ONE} from "./fixed/Q4x4.sol";
