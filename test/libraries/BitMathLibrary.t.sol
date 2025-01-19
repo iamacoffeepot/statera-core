aa9d35dc07178f2d3ee4bcdb0747b819d2b93e5d
iamacoffeepot <me@iamateapot.dev>
2025-01-19 07:28:02 -0700

    test: simple cases of find first set

--- test/libraries/BitMathLibrary.t.sol
+++ test/libraries/BitMathLibrary.t.sol
@@ -5,6 +5,12 @@ import {Test} from "forge-std/Test.sol";
 import {BitMathLibrary} from "../../src/libraries/BitMathLibrary.sol";
 
 contract BitMathLibraryTest is Test {
+    function test_find_first_set() external {
+        for (uint256 i = 0; i < 256; i++) {
+            assertEq(BitMathLibrary.ffs(1 << i), i);
+        }
+    }
+
     function test_fuzz_find_first_set(uint8 n, uint256 jitter) external {
         // Construct a random-ish input which always has the nth bit set with randomly set bits after those.
         // If the nth bit is the most significant bit then we do nothing as there are no bits to set after.
