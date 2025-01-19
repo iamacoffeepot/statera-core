ed9a6d0cd6cdf1e58ea369f5cc7ed77a777aa2cb
iamacoffeepot <me@iamateapot.dev>
2025-01-19 04:35:46 -0700

    feat: create pool stub

--- src/LibraPoolFactory.sol
+++ src/LibraPoolFactory.sol
@@ -1,3 +1,27 @@
 pragma solidity 0.8.27;
 
-contract LibraPoolFactory {}
+import {LibraPool} from "./LibraPool.sol";
+import {TokenizedVault} from "./interfaces/TokenizedVault.sol";
+
+
+import {
+    KernelError,
+    KernelErrorType
+} from "./types/Types.sol";
+
+contract LibraPoolFactory {
+    /// @custom:todo
+    mapping(TokenizedVault vault => LibraPool pool) public pools;
+
+    /// @notice Creates a pool for `vault`.
+    /// @notice
+    /// - Reverts with an `ILLEGAL_STATE` error if a pool already exists for `vault`.
+    /// @param vault The vault to create a pool for.
+    function createPool(TokenizedVault vault) external returns (LibraPool result) {
+        require(pools[vault] == LibraPool(address(0)), KernelError(KernelErrorType.ILLEGAL_STATE));
+
+        LibraPool pool = new LibraPool{salt: keccak256(abi.encode(vault))}();
+
+        return pool;
+    }
+}
