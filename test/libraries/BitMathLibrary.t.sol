40ae64e1482ee03d9f171db768f06549435d10c6
iamacoffeepot <me@iamateapot.dev>
2025-01-19 07:14:41 -0700

    test: find first set

---
+++ test/libraries/BitMathLibrary.t.sol
@@ -0,0 +1,14 @@
+pragma solidity 0.8.27;
+
+import {Test} from "forge-std/Test.sol";
+
+import {BitMathLibrary} from "../../src/libraries/BitMathLibrary.sol";
+
+contract BitMathLibraryTest is Test {
+    function test_fuzz_find_first_set(uint8 n, uint256 jitter) external {
+        // Construct a random-ish input which always has the nth bit set with randomly set bits after those.
+        // If the nth bit is the most significant bit then we do nothing as there are no bits to set after.
+        uint256 x = 1 << n | (n < 255 ? jitter << n + 1 : 0);
+        assertEq(BitMathLibrary.ffs(x), n);
+    }
+}
