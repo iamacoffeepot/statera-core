5fd0b5e7785c310893bc3daceeeca7144e2c1895
iamacoffeepot <me@iamateapot.dev>
2025-01-20 01:24:12 -0700

    fix: remove bitmap import

--- src/types/Types.sol
+++ src/types/Types.sol
@@ -5,5 +5,4 @@ import {Commitment} from "./Commitment.sol";
 import {KernelError, KernelErrorType} from "./KernelError.sol";
 import {LendingTerms, LendingTermsPacked} from "./LendingTerms.sol";
 import {Loan} from "./Loan.sol";
-import {BitmapX256} from "./bitmap/BitmapX256.sol";
 import {Q4x4, Q4X4_ONE} from "./fixed/Q4x4.sol";
