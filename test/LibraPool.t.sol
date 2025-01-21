84df3ce5d3b2d46fbebd05df337b9e29598fb37d
iamacoffeepot <me@iamateapot.dev>
2025-01-21 00:41:18 -0700

    test: supply liquidity increases supplied of recipient

--- test/LibraPool.t.sol
+++ test/LibraPool.t.sol
@@ -10,12 +10,40 @@ import {LibraPoolFactory} from "../src/LibraPoolFactory.sol";
 import {Token} from "../src/interfaces/Token.sol";
 import {TokenizedVault} from "../src/interfaces/TokenizedVault.sol";
 import {LendingTermsLibrary} from "../src/libraries/LendingTermsLibrary.sol";
-import {Q4x4} from "../src/types/Types.sol";
+
+
+import {
+    Commitment,
+    LendingTermsPacked,
+    Q4x4
+} from "../src/types/Types.sol";
 
 contract LibraPoolTest is Test {
     LibraPool public pool;
     Token public asset;
     TokenizedVault public vault;
+
+    modifier mintsAssetsTo(address owner, uint256 amount) {
+        deal(address(asset), owner, amount);
+        _;
+    }
+
+    modifier performsCallsAs(address caller) {
+        vm.startPrank(caller);
+        _;
+        vm.stopPrank();
+    }
+
+    modifier validatesSupplyLiquidityArguments(
+        Q4x4 borrowFactor,
+        Q4x4 profitFactor,
+        uint256 liquidity
+    ) {
+        vm.assume(LendingTermsLibrary.isValidBorrowFactor(borrowFactor));
+        vm.assume(LendingTermsLibrary.isValidProfitFactor(profitFactor));
+        vm.assume(liquidity > 0);
+        _;
+    }
 
     function setUp() external {
         vault = new MockTokenizedVault(asset = new MockToken());
@@ -24,22 +52,40 @@ contract LibraPoolTest is Test {
         pool = poolFactory.createPool(vault);
     }
 
-    function test_fuzz_supply_liquidity_increases_total_liquidity_supplied(
+    function test_fuzz_supply_liquidity_increases_supplied_of_recipient(
+        address caller,
         Q4x4 borrowFactor,
         Q4x4 profitFactor,
         uint256 liquidity,
         address recipient
-    ) external {
-        vm.assume(LendingTermsLibrary.isValidBorrowFactor(borrowFactor));
-        vm.assume(LendingTermsLibrary.isValidProfitFactor(profitFactor));
-        vm.assume(liquidity > 0);
-
-        deal(address(asset), address(this), liquidity);
-
+    ) external
+        validatesSupplyLiquidityArguments(borrowFactor, profitFactor, liquidity)
+        mintsAssetsTo(caller, liquidity)
+        performsCallsAs(caller)
+    {
         assertTrue(asset.approve(address(pool), liquidity));
-
         pool.supplyLiquidity(borrowFactor, profitFactor, liquidity, recipient);
 
+        (LendingTermsPacked terms, Commitment memory commitment) = pool.getCommitment(
+            recipient, borrowFactor, profitFactor
+        );
+
+        assertEq(commitment.liquiditySupplied, liquidity);
+    }
+
+    function test_fuzz_supply_liquidity_increases_total_liquidity_supplied(
+        address caller,
+        Q4x4 borrowFactor,
+        Q4x4 profitFactor,
+        uint256 liquidity,
+        address recipient
+    ) external
+        validatesSupplyLiquidityArguments(borrowFactor, profitFactor, liquidity)
+        mintsAssetsTo(caller, liquidity)
+        performsCallsAs(caller)
+    {
+        assertTrue(asset.approve(address(pool), liquidity));
+        pool.supplyLiquidity(borrowFactor, profitFactor, liquidity, recipient);
         assertEq(pool.totalLiquiditySupplied(), liquidity);
     }
 }
