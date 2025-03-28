dd0dab72abc87377ac5aec92fd00ffd8f70cbf84
iamacoffeepot <me@iamateapot.dev>
2025-03-28 17:09:26 -0600

    doc: note about finality

--- src/types/Bucket.sol
+++ src/types/Bucket.sol
@@ -4,5 +4,7 @@ struct Bucket {
     uint256 liquidityBorrowed;
     uint256 liquiditySupplied;
     uint256 liquidityWeighted;
+    /// @dev This value is not finalized until after the pool expires or all open loans on a bucket are closed.
+    /// @custom:todo The conditions of finalization will change as the design changes and the note should be updated.
     uint256 supplierProfitsRealized;
 }
