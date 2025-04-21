19cbb60eb1fd82caac043d17e7ddc819de338128
iamacoffeepot <me@iamateapot.dev>
2025-04-21 12:05:46 -0600

    doc: total shares supplied invariant

--- src/StateraPool.sol
+++ src/StateraPool.sol
@@ -60,6 +60,7 @@ contract StateraPool {
     uint256 public totalLoans;
 
     /// @custom:todo
+    /// @custom:invariant `totalSharesSupplied >= ΣsharesSupplied`
     uint256 public totalSharesSupplied;
 
     /// @custom:todo
