26c0028f95d9ba79693ab2532ac7725767c3c65a
iamacoffeepot <me@iamateapot.dev>
2025-04-21 16:45:46 -0600

    refactor: use muldiv for u256 and q4x4 multiplication

--- src/libraries/FixedPointMathLibrary.sol
+++ src/libraries/FixedPointMathLibrary.sol
@@ -1,5 +1,6 @@
 pragma solidity 0.8.27;
 
+import {MathLibrary} from "./MathLibrary.sol";
 import {Q4x4} from "../types/Types.sol";
 
 /// @dev Alias of `type(uint8).max` for use in inline assembly.
@@ -9,10 +10,6 @@ uint256 constant UINT8_MAXIMUM = 0xff;
 library FixedPointMathLibrary {
     /// @notice Multiplies an unsigned 256 bit integer by a binary fixed point number with 4 integer bits and 4 fraction bits.
     function multiplyByQ4x4(uint256 n, Q4x4 q) internal pure returns (uint256 result) {
-        require(n < 2**248);
-
-        assembly {
-            result := shr(4, mul(and(UINT8_MAXIMUM, q), n))
-        }
+        result = MathLibrary.mulDiv(n, Q4x4.unwrap(q), 1 << 4);
     }
 }
